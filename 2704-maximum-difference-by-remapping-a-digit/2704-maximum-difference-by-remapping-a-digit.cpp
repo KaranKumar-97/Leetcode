550bb6210f38881915ddc1a74ea4a82ce8f0bc75
Karan Kumar <105391077+KaranKumar-97@users.noreply.github.com>
2025-06-15 17:45:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.19%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        int x1 = stoi(to_string(num).substr(0,1));
+        int x2 = stoi(to_string(num).substr(0,1));
+        int y1 = 0, y2 = 9;
+
+        if(x2==9) {
+            string numstr = to_string(num);
+            for(char ch : numstr){
+                if(ch-'0' < x2){ 
+                    x2= ch-'0';
+                    break;
+                };
+            }
+        }
+
+        string n1 = to_string(num);
+        string n2 = to_string(num);
+
+        for(int i=0;i<n1.size();i++){
+            if(n1[i] == x1 + '0'){
+                n1[i] = y1 + '0';
+            }
+        }
+        for(int i=0;i<n2.size();i++){
+            if(n2[i] == x2 + '0'){
+                n2[i] = y2 +'0';
+            }
+        }
+
+        int num1 = stoi(n1);
+        int num2 = stoi(n2);
+
+        return abs(num2-num1);     
+    }
+};
